d09ef1415a897a6bd884acea0549b8bfacfbd23a
Daniel Hood <hood.danielc@gmail.com>
2018-11-01 00:14:25 -0700

    Remove user docker interactive flag for jenkins

--- scripts/build_in_container.sh
+++ scripts/build_in_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 "$( dirname "${DIR}" )/scripts/build_container.sh"
-docker run --rm -ti dhoodlum/yourhtml /opt/yourhtml/scripts/build.sh
+docker run --rm -t dhoodlum/yourhtml /opt/yourhtml/scripts/build.sh

51e35fb27014e6976e15343e786666df204690f4
Daniel Hood <hood.danielc@gmail.com>
2018-11-04 16:03:25 -0800

    Add clang code coverage report to build
    Add llvm to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM base/devel
 
 RUN pacman -Syyu --noconfirm
-RUN pacman -S clang python2 git --noconfirm
+RUN pacman -S clang llvm python2 git --noconfirm
 RUN git clone https://github.com/JasonL9000/ib.git /opt/ib
 RUN chmod +x /opt/ib/ib
 ENV PATH /opt/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/ib
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 TEST_DIR="$( dirname "${DIR}" )/test"
 cd "$( dirname "${DIR}" )"
-ib --test_all test
+
+OUT_DIR="../out"
+
+if [ -d "${OUT_DIR}/coverage" ]; then
+  rm -rf "${OUT_DIR}/coverage"
+fi
+
+mkdir -p "${OUT_DIR}/coverage"
+LLVM_PROFILE_FILE="${OUT_DIR}/coverage/coverage-%p.profraw" \
+  ib --cfg test --test_all test
+
+FILE_LIST=(../out/coverage/*.profraw)
+TARGET_LIST=(../out/test/test/*-test)
+
+llvm-profdata merge -sparse "${FILE_LIST[@]}" -o "${OUT_DIR}/coverage/all.profdata"
+llvm-cov show "${TARGET_LIST[@]}" -instr-profile="${OUT_DIR}/coverage/all.profdata"
+llvm-cov report "${TARGET_LIST[@]}" -instr-profile="${OUT_DIR}/coverage/all.profdata"
